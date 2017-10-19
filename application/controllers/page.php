7231ddb80588a4e028ff7392ca36e68edb2fa99b
alfianff25 <cukuptauajah@gmail.com>
2017-10-19 23:18:24 +0700

    Add files via upload

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -4,13 +4,13 @@ class Page extends CI_Controller {
 	
 public function __construct() {
             parent::__construct();
-            $this->load->model('mymodel');
+            $this->load->model('Mymodel');
              $this->load->library('session');
-            if($this->session->userdata('status') != "login"){
-			redirect(base_url('index.php/page/home'));
+   //          if($this->session->userdata('status') != "login"){
+			// redirect(base_url('index.php/page/home'));
         //}
 			}
-}
+
 
 	public function index() {
 		$this->load->view('sign');
@@ -56,6 +56,16 @@ public function __construct() {
 		 }
 		$this->load->view('header');
 		$this->load->view('form'); 
+	}
+
+	public function list(){
+		if($this->session->userdata('status') != "login" || $this->session->userdata('status') == "" || $this->session->userdata('status') == null ) {
+		 	redirect(base_url());
+		}
+		$this->load->view('header');
+		$data['mahasiswa'] = $this->Mymodel->all();
+		$data['user'] = $this->Mymodel->all1();
+		$this->load->view('list', $data);
 	}
 
 	
